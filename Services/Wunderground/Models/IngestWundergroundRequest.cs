9b347fd13a4b670aea464c28607d364b54fbaca5
alex182 <alexbaker182@gmail.com>
2023-08-26 16:55:50 -0500

    cleaning up code; can ingest station data to influx db

---
+++ Services/Wunderground/Models/IngestWundergroundRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace LakeStatsApi.Services.Wunderground.Models
+{
+    public class IngestWundergroundRequest
+    {
+        public string StationId { get; set; }
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+        public double Dewpoint { get; set; }
+        public double WindChill { get; set; }
+        public int WindDirection { get; set; }
+        public double WindSpeed { get; set; }
+        public double WindGust { get; set; }
+        public double TotalRain { get; set; }
+        public double DailyRain { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public string CorrelationId { get; set; }
+    }
+}
